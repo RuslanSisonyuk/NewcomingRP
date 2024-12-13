4862ea5b534bc78d636726007538d65c93b8026c
Ruslan <randomrocketgalaxy@gmail.com>
2024-12-13 12:59:55 +0200

    Burger menu now properly disapears on link redirect

--- scripts/home.js
+++ scripts/home.js
@@ -53,12 +53,10 @@ $("div").on("click",".pagelinkhome", function(event){
 });
 
 $('div.top-navbar-burgermenu-button').on("click","img",function(event){
-   console.log($('.top-navbar-burgermenu-contents').css("display"));
    if($('.top-navbar-burgermenu-contents').css("display")=="none"){
-      $('div.main-container').css("opacity","0");
+      $('#main-content').css("opacity","0");
       setTimeout(() => {
-         $('.main-container').css("display","none");
-         // $('div.main-content').css("display","none");
+         $('#main-content').css("display","none");
          $('.top-navbar-burgermenu-contents').css("display","flex");
          $('.top-navbar-burgermenu-contents').css("opacity","1");
       }, 600);
@@ -67,10 +65,19 @@ $('div.top-navbar-burgermenu-button').on("click","img",function(event){
       $('.top-navbar-burgermenu-contents').css("opacity","0");
       setTimeout(() => {
          $('.top-navbar-burgermenu-contents').css("display","none");
-         $('.main-container').css("display","flex");
-         $('div.main-container').css("opacity","1");
-
-         // $('div.main-content').css("display","flex");
+         $('#main-content').css("opacity","1");
+         $('#main-content').css("display","flex");
       }, 600);
    }
+});
+
+$('div.top-navbar-burgermenu-contents').on("click",".burgermenu-pagelink",function(event){
+   console.log("success1");
+   $('div.top-navbar-burgermenu-contents').css('opacity',"0");
+   $('#main-content').css("display","flex");
+   setTimeout(() => {
+      $('div.top-navbar-burgermenu-contents').css('display',"none");
+      $('#main-content').css("opacity","1");
+   }, 600);
+
 });
