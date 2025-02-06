f33535c236c4ed57d0d1b4b8f5e205815704d49c
Ruslan <randomrocketgalaxy@gmail.com>
2025-02-06 11:28:38 +0200

    moved page_redirect script into home, added page backtracking in home script

--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,7 @@
 // document.getElementById("home-title-div").addEventListener
+
+let page_history=[];
+
 $("div").on("click","#home-title-div", function(){
    document.getElementById("home-title-div").style.opacity = "0";
    setTimeout(() => {
@@ -29,28 +32,28 @@ $("div").on("click",".homepage-link", function(event){
 });
 
 
-$("div").on("click",".pagelinkhome", function(event){
-   event.stopPropagation();
-   event.preventDefault();
-   $("div.nav-item").css("opacity","0");
-   $("div.main-container").css("opacity","0");
+// $("div").on("click",".pagelinkhome", function(event){
+//    event.stopPropagation();
+//    event.preventDefault();
+//    $("div.nav-item").css("opacity","0");
+//    $("div.main-container").css("opacity","0");
 
-   setTimeout(() => {
-      let url = event.target.getAttribute("href");
-      fetch(url)
-      .then(response=>{
-         if(!response.ok) 
-            throw new Error('Page not found');
-         return response.text();
-      })
-      .then(data => {
-         console.log("success");
-         $("div.nav-item").css("display","none");
-         document.getElementById("main-content").innerHTML = data;
-      })
-      .catch(error => console.error("Failed catching content: ",error));
-   }, 600);
-});
+//    setTimeout(() => {
+//       let url = event.target.getAttribute("href");
+//       fetch(url)
+//       .then(response=>{
+//          if(!response.ok) 
+//             throw new Error('Page not found');
+//          return response.text();
+//       })
+//       .then(data => {
+//          console.log("success");
+//          $("div.nav-item").css("display","none");
+//          document.getElementById("main-content").innerHTML = data;
+//       })
+//       .catch(error => console.error("Failed catching content: ",error));
+//    }, 600);
+// });
 
 $('div.top-navbar-burgermenu-button').on("click","img",function(event){
    if($('.top-navbar-burgermenu-contents').css("display")=="none"){
@@ -85,4 +88,59 @@ $('div.top-navbar-burgermenu-contents').on("click",".burgermenu-pagelink",functi
 
 $('div').on('click','.dropdown-section',function(){
    $(this).children('p').toggle();
+});
+
+$( "div" ).on("click",".pagelink", function(event){
+   console.log("Event triggered");
+   event.stopPropagation();
+   event.preventDefault();
+   window.scrollTo({ top: 0, behavior: 'smooth' });
+
+   $('.main-container').css("opacity",'0');
+   setTimeout(() => {
+      let url = event.target.getAttribute("href");
+      console.log(url);
+      fetch(url)
+      .then(response => {
+         if(!response.ok) 
+            throw new Error('Page not found');
+         return response.text();
+      })
+      .then(data => {
+         document.getElementById("main-content").innerHTML = data;
+         page_history.push(url);
+      })
+      .catch(error => {console.error('Error catching content: ',error)});
+   }, 800);
+});
+
+$('div').on("click","#page-contents-list li a",function(event){
+   event.stopPropagation();
+   event.preventDefault();
+   let element = event.target.getAttribute('href');
+   console.log("Scrolling to section.. "+element);
+   $('html, body').animate({
+      scrollTop: document.getElementById(element).offsetTop
+   }, 600);
+});
+
+$('div').on("click",".pagelinkhome",function(event){
+   event.stopPropagation();
+   event.preventDefault();
+   let url;
+   if(page_history.length>1){
+      url = page_history[page_history.length-2];
+      console.log(url);
+      fetch(url)
+      .then(response => {
+         if(!response.ok)
+         throw new Error("Page not found");
+         return response.text();
+      })
+      .then(data => {
+         document.getElementById("main-content").innerHTML = data;
+         page_history.pop();
+      })
+      .catch(error => {console.error('Error catching content: ',error)});
+   }
 });
